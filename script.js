efc8165e24f8a9ff6a528dc32fad169273254a55
Ayla Delavi <ayladelavi@gmail.com>
2025-06-03 16:56:17 -0300

    Atualização na primeira seção, adição de carrossel de imagens com JS

--- script.js
+++ script.js
@@ -1,6 +1,40 @@
-let count = 1;
-  setInterval(() => {
-    document.getElementById('radio' + count).checked = true;
-    count++;
-    if (count > 5) count = 1;
-  }, 4000); // troca a cada 4 segundos
+const imagens = [
+    "./images/Encontre seu novo.png",
+    "./images/banner1.png",
+    "./images/banner2.png",
+  ];
+
+  let index = 0;
+
+  function criarImagem(src, classeExtra = "") {
+    const div = document.createElement("div");
+    div.classList.add("carrossel-imagem");
+    if (classeExtra) div.classList.add(classeExtra);
+
+    const img = document.createElement("img");
+    img.src = src;
+    div.appendChild(img);
+
+    return div;
+  }
+
+  function atualizarCarrossel() {
+    const carrossel = document.querySelector(".carrossel");
+    carrossel.innerHTML = "";
+
+    const anterior = imagens[(index - 1 + imagens.length) % imagens.length];
+    const atual = imagens[index % imagens.length];
+    const proximo = imagens[(index + 1) % imagens.length];
+
+    carrossel.appendChild(criarImagem(anterior, "esquerda"));
+    carrossel.appendChild(criarImagem(atual, "destaque"));
+    carrossel.appendChild(criarImagem(proximo, "direita"));
+  }
+
+  function proximaImagem() {
+    index = (index + 1) % imagens.length;
+    atualizarCarrossel();
+  }
+
+  atualizarCarrossel();
+  setInterval(proximaImagem, 5000);
